b659fc8e1a0834b1ee6ee60c762124d687a0388c
Ertuğrul Badem <ertugrulbdm@gmail.com>
2025-11-01 20:42:44 +0300

    fix vercel deploy two

--- components/contacts/Maps.tsx
+++ components/contacts/Maps.tsx
@@ -10,7 +10,7 @@ interface MapsProps {
 
 
 
-const Maps: React.FC<MapsProps> = ({ lat, lng, zoom = 15 }) => {
+const Maps: React.FC<MapsProps> = () => {
   const mapSrc = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96220.94003190826!2d28.719064497265638!3d41.0929359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caaf7ff3259229%3A0x3638eb5a1a62619a!2sTKM!5e0!3m2!1str!2str!4v1761398578518!5m2!1str!2str`;
 
   return (
